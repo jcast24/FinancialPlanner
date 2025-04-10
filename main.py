272b459115a81c4a5c36415566b8b769d8ad5403
Jesus Castro <castrojesus39@gmail.com>
2025-04-09 17:19:07 -0700

    included the list_all_records and insert_data menu options

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
+from datetime import datetime
+import database
+
+def get_date() -> str:
+    now = datetime.now()
+    date = now.strftime("%d%m%Y")
+    return date
+
 def calculate_monthly_spending() -> list[float]:
     payAmount = float(input("Enter the amount for your paycheck: "))
-    NECESSITIES = 0.50
+    NEEDS = 0.50
     WANTS = 0.30
     SAVINGS = 0.20
 
-    final_n = payAmount * NECESSITIES
+    final_n = payAmount * NEEDS
     final_w = payAmount * WANTS 
     final_s = payAmount * SAVINGS
+    
+    print(f"Your pay is: {payAmount}")
+
     return [final_n, final_w, final_s]
 
 def menu():
     isRunning = True
-    print("1. List all records\n2. New Input\n3. Update Record\n4. Delete Record\n5. Quit")
+    print("1. List all records\n2. New Input\n3. Update Record\n4. Delete Record\n5. DisplayVisualization\n6. Quit")
 
     while isRunning:
         userOption = int(input("What would you like to do? "))
         match userOption:
             case 1:
-                print("List all records")
+                database.list_all_records()
                 break
             case 2:
-                print("New input")
+                database.insert_data()
                 break
             case 3:
                 print("Update record")
@@ -29,6 +40,9 @@ def menu():
                 print("Delete record")
                 break
             case 5:
+                print("Display visualization")
+                break
+            case 6:
                 print("Quit")
                 isRunning = False
                 break;
@@ -37,10 +51,7 @@ def menu():
 
 def main():
     print("Welcome to your personal financial planner!")
-    # menu()
-
-    result = calculate_monthly_spending()
-    print(result)
+    menu()
 
 if __name__ == "__main__":
     main()
