4966b62c06d279909d39e7a84983171b42752bc8
Jesus Castro <castrojesus39@gmail.com>
2025-04-09 17:18:29 -0700

    insert_data and list_all_records implemented

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import sqlite3
 
+from main import calculate_monthly_spending, get_date
+
 def db_connection(db_name):
     conn = sqlite3.connect(db_name)
     return conn
@@ -31,3 +33,39 @@ def create_table():
         # commit the changes
         connection.commit()
         # print("Table 'finances' has been created!")
+
+def insert_data():
+    monthly_spending = calculate_monthly_spending()
+    needs = monthly_spending[0]
+    wants = monthly_spending[1]
+    savings = monthly_spending[2]
+
+    date = get_date()
+    
+    data = (savings, wants, needs, date)
+
+    with db_connection('finances.db') as connection:
+        cursor = connection.cursor()
+
+        insert_query = '''
+            INSERT INTO Finances (savings, wants, needs, date)
+            VALUES (?, ?, ?, ?)
+        '''
+
+        cursor.execute(insert_query, data)
+        print("Record inserted successfully!")
+
+def list_all_records():
+    with db_connection('finances.db') as connection:
+        cursor = connection.cursor()
+        select_query = '''
+            SELECT * FROM Finances;
+        '''
+        cursor.execute(select_query)
+
+        # In the future find a better way of representing this data to
+        # make it look better
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        
